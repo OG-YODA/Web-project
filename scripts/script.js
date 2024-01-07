0c5e66213ded382f557f9c916066176598b79a26
OG-YODA <116004568+OG-YODA@users.noreply.github.com>
2024-01-07 20:57:02 +0100

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const languageToggleBtn = document.getElementById("languageToggle");
-  const languageDropdown = document.getElementById("languageDropdown");
-  const defaultLanguage = "ru";
-  let currentLanguage = defaultLanguage;
-
-  function updateTextContent() {
-      const elements = document.querySelectorAll("[data-translate]");
-      elements.forEach((element) => {
-          const key = element.getAttribute("data-translate");
-          if (translations[currentLanguage] && translations[currentLanguage][key]) {
-              element.textContent = translations[currentLanguage][key];
-          }
-      });
-  }
-
-  function changeLanguage(lang) {
-      currentLanguage = lang;
-      updateTextContent();
-      // Скрываем выпадающий список после выбора языка
-      languageDropdown.style.display = "none";
-  }
-
-  // Обработка выбора языка
-  languageDropdown.addEventListener("click", function (event) {
-      if (event.target.tagName === "A") {
-          const selectedLanguage = event.target.getAttribute("data-language");
-          changeLanguage(selectedLanguage);
-      }
+document.addEventListener('DOMContentLoaded', function () {
+  const languageDropdown = document.querySelector('.dropdown');
+  const menuItems = document.querySelectorAll('.menu-item');
+  
+  languageDropdown.addEventListener('click', function () {
+      this.classList.toggle('active');
   });
 
-  updateTextContent();
+  document.querySelectorAll('.dropdown-content a').forEach(function (link) {
+      link.addEventListener('click', function (event) {
+          event.preventDefault();
+          const selectedLanguage = this.getAttribute('data-language');
+          translatePage(selectedLanguage);
+          languageDropdown.classList.remove('active');
+      });
+  });
+
+  function translatePage(language) {
+      const translatedTexts = translations[language];
+
+      if (translatedTexts) {
+          Object.keys(translatedTexts).forEach(function (key) {
+              const elements = document.querySelectorAll(`[data-translate="${key}"]`);
+              elements.forEach(function (element) {
+                  element.innerHTML = translatedTexts[key];
+              });
+          });
+      }
+  }
 });
