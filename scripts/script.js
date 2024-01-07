ac74138f2cdfe57320118e9581670e4edb38184e
OG-YODA <116004568+OG-YODA@users.noreply.github.com>
2024-01-07 23:25:27 +0100

    refactoring & translations update

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,38 @@
+// scripts/script.js
+
 document.addEventListener('DOMContentLoaded', function () {
-  const languageDropdown = document.querySelector('.dropdown');
-  const menuItems = document.querySelectorAll('.menu-item');
-  
-  languageDropdown.addEventListener('click', function () {
-      this.classList.toggle('active');
-  });
+    const languageDropdown = document.querySelector('.dropdown');
+    const menuItems = document.querySelectorAll('.menu-item');
 
-  document.querySelectorAll('.dropdown-content a').forEach(function (link) {
-      link.addEventListener('click', function (event) {
-          event.preventDefault();
-          const selectedLanguage = this.getAttribute('data-language');
-          translatePage(selectedLanguage);
-          languageDropdown.classList.remove('active');
-      });
-  });
+    languageDropdown.addEventListener('click', function () {
+        this.classList.toggle('active');
+    });
 
-  function translatePage(language) {
-      const translatedTexts = translations[language];
+    document.querySelectorAll('.dropdown-content a').forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const selectedLanguage = this.getAttribute('data-language');
+            
+            // Save selected language to URL parameter
+            const url = new URL(window.location.href);
+            url.searchParams.set('lang', selectedLanguage);
+            window.history.pushState({}, '', url);
 
-      if (translatedTexts) {
-          Object.keys(translatedTexts).forEach(function (key) {
-              const elements = document.querySelectorAll(`[data-translate="${key}"]`);
-              elements.forEach(function (element) {
-                  element.innerHTML = translatedTexts[key];
-              });
-          });
-      }
-  }
+            translatePage(selectedLanguage);
+            languageDropdown.classList.remove('active');
+        });
+    });
+
+    function translatePage(language) {
+        const translatedTexts = translations[language];
+
+        if (translatedTexts) {
+            Object.keys(translatedTexts).forEach(function (key) {
+                const elements = document.querySelectorAll(`[data-translate="${key}"]`);
+                elements.forEach(function (element) {
+                    element.innerHTML = translatedTexts[key];
+                });
+            });
+        }
+    }
 });
